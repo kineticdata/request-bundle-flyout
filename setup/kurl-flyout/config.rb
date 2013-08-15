6a41b96941205b5ceb0dcf5ab0e07f6185d2f4fa
Shayne Koestler <shayne.koestler@kineticdata.com>
2013-08-15 18:44:36 -0500

    unzipping the kurl code in the setup dir

---
+++ setup/kurl-flyout/config.rb
@@ -0,0 +1,30 @@
+CATALOG_NAME = "Flyout"
+DISPLAY_NAME_FOR_URL = "Flyout"
+
+CATALOG_SERVICE_CATALOG_NAME = "#{CATALOG_NAME} Service Portal"
+CATALOG_NAME_URL = "#{DISPLAY_NAME_FOR_URL}_Catalog"
+STANDARD_AUTHENTICATION_FORM = "Catalog"
+
+# Used to drive the standard look and feel
+THEMES_BASE = "/themes/flyout"
+DISPLAY_PAGE = "#{THEMES_BASE}/packages/base/display.jsp"
+CONFIRMATION_PAGE = "#{THEMES_BASE}/packages/base/confirmation.jsp"
+REVIEW_PAGE = "#{THEMES_BASE}/packages/base/review.jsp"
+ABOUT_PAGE = "#{THEMES_BASE}/packages/catalog/about.jsp"
+CATALOG_PAGE = "#{THEMES_BASE}/packages/catalog/catalog.jsp"
+CATALOG_LISTING_PAGE = "#{THEMES_BASE}/packages/catalog/catalogListing.jsp"
+CATEGORY_PAGE = "#{THEMES_BASE}/packages/catalog/category.jsp"
+SEARCH_PAGE = "#{THEMES_BASE}/packages/catalog/search.jsp"
+SUBMISSIONS_PAGE = "#{THEMES_BASE}/packages/submissions/submissions.jsp"
+
+# Used in Network Service Items - for domain name mgmt
+COMPANY_BASE_DOMAIN_NAME = "kineticdata.com"
+
+WEB_SERVER = "http://matrix.kineticdata.com/kinetic/"
+
+DAATA_SET = "SYSTEM_DEFAULTS"
+HEADER_CONTENT = %||
+
+ASSIGNEE= "0;"
+MANAGEMENT_GROUP = "Public"
+SUBMISSION_GROUP = "Public"
